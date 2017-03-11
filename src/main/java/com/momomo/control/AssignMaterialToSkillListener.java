a3c7bfde6920455b62a433dac5084efc774d3647
Charles Bergeron <charlesbergeron@cmail.carleton.ca>
2017-03-11 12:23:00 -0500

    Added new UIs, assigning materials to skill is broken @Daniel

---
+++ src/main/java/com/momomo/control/AssignMaterialToSkillListener.java
@@ -0,0 +1,7 @@
+package com.momomo.control;
+
+/**
+ * Created by Charberg on 3/11/2017.
+ */
+public class AssignMaterialToSkillListener {
+}
---
+++ src/main/java/com/momomo/view/SkillManPage.java
@@ -0,0 +1,7 @@
+package com.momomo.view;
+
+/**
+ * Created by Charberg on 3/11/2017.
+ */
+public class SkillManPage {
+}
